2e57049d38921f9ae6c514f215c7582494029494
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-23 15:28:36 -0500

    Pass header from cookies to all pages that require it

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const cookieParser = require('cookie-parser')
+
 const app = express()
 const PORT = 8080 // default port 8080
 
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
@@ -23,7 +26,10 @@ app.get("/hello", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  }
   res.render("urls_index", templateVars)
 })
 
@@ -42,11 +48,18 @@ app.post("/urls", (req, res) => {
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars)
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
+  }
   res.render("urls_show", templateVars)
 })
 
