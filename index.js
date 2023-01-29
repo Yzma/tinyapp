7d344c3c04cf5a554c72dfd41b2617711483afed
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-29 11:29:57 -0500

    Make index route user to /login instead of /urls (to prevent being routed to an error message)

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(methodOverride('_method'))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-  return res.redirect('/urls')
+  return res.redirect('/login')
 })
 
 app.use(debugRoute)
